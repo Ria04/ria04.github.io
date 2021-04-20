9712dc02cf1605c8ff31eeff441ffc9b1c3d0d6d
riya <terkhedkarriya09@gmail.com>
2021-04-20 13:51:08 +0530

    popup elements added

--- scripts/index.js
+++ scripts/index.js
@@ -1,103 +1,102 @@
-function displayVideo(){
-    var element=document.getElementById("popupVideo");
-    element.style.display="flex";
+function displayVideo() {
+  var element = document.getElementById("popupVideo");
+  element.style.display = "flex";
 }
-function hideVideo(){
-    var element=document.getElementById("popupVideo");
-    element.style.display="none";
+function hideVideo() {
+  var element = document.getElementById("popupVideo");
+  element.style.display = "none";
 }
-
-function openInput(id){
-    var element=document.getElementById("button-component"+id);
-    var close=document.getElementById("close-button"+id);
-    var childs=element.getElementsByClassName("button-item");
-    for(var i=0;i<childs.length;i++){
-        childs[i].classList.add("shrink-button-component");
-    }
-    close.style.display="block";
+function displayPopupForm() {
+  var element = document.getElementById("popupForm");
+  element.style.display = "flex";
 }
-
-
-
-function closeInput(id){
-    var element=document.getElementById("button-component"+id);
-    var childs=element.getElementsByClassName("button-item");
-    console.log("abcde")
-    for(var i=0;i<childs.length;i++){
-        childs[i].classList.remove("shrink-button-component");
-    }
-    document.getElementById("close-button"+id).style.display="none";
-
+function hidePopupForm() {
+  var element = document.getElementById("popupForm");
+  element.style.display = "none";
+}
+function displayCertificate() {
+  var element = document.getElementById("popupCertificate");
+  element.style.display = "flex";
+}
+function hideCertificate() {
+  var element = document.getElementById("popupCertificate");
+  element.style.display = "none";
 }
 
-function showDescription(id){
-    var element=document.getElementById(id);
-    var icon=document.getElementById("icon"+id);
-    if(element.style.display!="block"){
-        element.style.display="block";
-        icon.classList.replace("fa-arrow-down","fa-arrow-up");
-    }
-    else{
-        element.style.display="none";
-        icon.classList.replace("fa-arrow-up","fa-arrow-down");
+function openInput(id) {
+  var element = document.getElementById("button-component" + id);
+  var close = document.getElementById("close-button" + id);
+  var childs = element.getElementsByClassName("button-item");
+  for (var i = 0; i < childs.length; i++) {
+    childs[i].classList.add("shrink-button-component");
+  }
+  close.style.display = "block";
+}
 
-    }
-    
+function closeInput(id) {
+  var element = document.getElementById("button-component" + id);
+  var childs = element.getElementsByClassName("button-item");
+  console.log("abcde");
+  for (var i = 0; i < childs.length; i++) {
+    childs[i].classList.remove("shrink-button-component");
+  }
+  document.getElementById("close-button" + id).style.display = "none";
+}
+
+function showDescription(id) {
+  var element = document.getElementById(id);
+  var icon = document.getElementById("icon" + id);
+  if (element.style.display != "block") {
+    element.style.display = "block";
+    icon.classList.replace("fa-arrow-down", "fa-arrow-up");
+  } else {
+    element.style.display = "none";
+    icon.classList.replace("fa-arrow-up", "fa-arrow-down");
+  }
 }
 
 function currentSlide(n) {
-    var head = document.getElementById("technology-parent");
-    var slides = head.getElementsByClassName("technology-wrap");
+  var head = document.getElementById("technology-parent");
+  var slides = head.getElementsByClassName("technology-wrap");
 
-    for (var s = 0; s < slides.length; s++) {
-        slides[s].style.display = "none";
-    }
-    slides[n].style.display = "flex";
-
+  for (var s = 0; s < slides.length; s++) {
+    slides[s].style.display = "none";
+  }
+  slides[n].style.display = "flex";
 }
 
 currentSlide(0);
 
-
-
-
 var carouselhead = document.getElementById("carousel");
 var dots = carouselhead.getElementsByTagName("span");
 for (var j = 0; j < dots.length; j++) {
-    dots[j].addEventListener("click", function () {
-        var currentdot = document.getElementsByClassName("active");
-        currentdot[0].className = currentdot[0].className.replace("active", " ");
-        this.className += "active";
-    });
+  dots[j].addEventListener("click", function () {
+    var currentdot = document.getElementsByClassName("active");
+    currentdot[0].className = currentdot[0].className.replace("active", " ");
+    this.className += "active";
+  });
 }
 
-
-function showInnerItem(id){
-    var element=document.getElementById(id);
-    var icon=document.getElementById("icon"+id);
-    if(element.style.display!="flex"){
-        element.style.display="flex";
-        icon.classList.replace("fa-angle-down","fa-angle-up");
-    }
-    else{
-        element.style.display="none";
-        icon.classList.replace("fa-angle-up","fa-angle-down");
-
-    }
-    
+function showInnerItem(id) {
+  var element = document.getElementById(id);
+  var icon = document.getElementById("icon" + id);
+  if (element.style.display != "flex") {
+    element.style.display = "flex";
+    icon.classList.replace("fa-angle-down", "fa-angle-up");
+  } else {
+    element.style.display = "none";
+    icon.classList.replace("fa-angle-up", "fa-angle-down");
+  }
 }
 
-function showFaq(id){
-    var element=document.getElementById(id);
-    var icon=document.getElementById("icon"+id);
-    if(element.style.display!="flex"){
-        element.style.display="flex";
-        icon.classList.replace("fa-angle-down","fa-angle-up");
-    }
-    else{
-        element.style.display="none";
-        icon.classList.replace("fa-angle-up","fa-angle-down");
-
-    }
-    
+function showFaq(id) {
+  var element = document.getElementById(id);
+  var icon = document.getElementById("icon" + id);
+  if (element.style.display != "flex") {
+    element.style.display = "flex";
+    icon.classList.replace("fa-angle-down", "fa-angle-up");
+  } else {
+    element.style.display = "none";
+    icon.classList.replace("fa-angle-up", "fa-angle-down");
+  }
 }
